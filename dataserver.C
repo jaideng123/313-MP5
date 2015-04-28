2ac1a9555d84f1b5785499977209561a93d43bdc
jaideng123 <jaideng123@yahoo.com>
2015-04-28 11:00:23 -0500

    Added more stuff

--- dataserver.C
+++ dataserver.C
@@ -33,6 +33,7 @@
 #include <stdlib.h>
 
 #include "reqchannel.H"
+#include "NetworkRequestChannel.H"
 
 using namespace std;
 
@@ -69,23 +70,22 @@ string int2string(int number) {
    ss << number;//add number to the stream
    return ss.str();//return a string with the contents of the stream
 }
+void process_hello(int sock, string & _request) {
+	char* data = &_request[0];
+	write(sock,data,strlen(_request.c_str())+1);
+}
 
+void process_data(int sock, const string &  _request) {
+  usleep(1000 + (rand() % 5000));
+  string r = int2string(rand() % 100);
+  char* data = &r[0];
+  write(sock,data,strlen(r.c_str())+1);
+}
 /*--------------------------------------------------------------------------*/
 /* LOCAL FUNCTIONS -- THREAD FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-void * handle_data_requests(void * args) {
 
-  RequestChannel * data_channel =  (RequestChannel*)args;
-
-  // -- Handle client requests on this channel. 
-  
-  handle_process_loop(*data_channel);
-
-  // -- Client has quit. We remove channel.
- 
-  delete data_channel;
-}
 void* connection_handler(void* args) {
   int socket = *(int*)args;
   int continue_processing = 1;
@@ -98,19 +98,15 @@ void* connection_handler(void* args) {
     } 
         
     string request = buf;
-    // if (_request.compare(0, 5, "hello") == 0) {
-    //   process_hello(_channel, _request);
-    // }
-    // else if (_request.compare(0, 4, "data") == 0) {
-    //   process_data(_channel, _request);
-    // }
-    // else if (_request.compare(0, 9, "newthread") == 0) {
-    //   process_newthread(_channel, _request);
-    // }
-    // else {
-    //   _channel.cwrite("unknown request");
-    // }
-
+	if (request.compare(0, 4, "quit") == 0) {
+		continue_processing = 0;
+	}
+	else if (request.compare(0, 5, "hello") == 0) {
+		process_hello(socket, request);
+	}
+	else if (request.compare(0, 4, "data") == 0) {
+		process_data(socket, request);
+	}
   }
 
 }
@@ -119,84 +115,6 @@ void* connection_handler(void* args) {
 /* LOCAL FUNCTIONS -- INDIVIDUAL REQUESTS */
 /*--------------------------------------------------------------------------*/
 
-void process_hello(RequestChannel & _channel, const string & _request) {
-  _channel.cwrite("hello to you too");
-}
-
-void process_data(RequestChannel & _channel, const string &  _request) {
-  usleep(1000 + (rand() % 5000));
-  //_channel.cwrite("here comes data about " + _request.substr(4) + ": " + int2string(random() % 100));
-  _channel.cwrite(int2string(rand() % 100));
-}
-
-void process_newthread(RequestChannel & _channel, const string & _request) {
-  int error;
-  nthreads ++;
-
-  // -- Name new data channel
-
-  string new_channel_name = "data" + int2string(nthreads) + "_";
-  //  cout << "new channel name = " << new_channel_name << endl;
-
-  // -- Pass new channel name back to client
-
-  _channel.cwrite(new_channel_name);
-
-  // -- Construct new data channel (pointer to be passed to thread function)
-  
-  RequestChannel * data_channel = new RequestChannel(new_channel_name, RequestChannel::SERVER_SIDE);
-
-  // -- Create new thread to handle request channel
-
-  pthread_t thread_id;
-  //  cout << "starting new thread " << nthreads << endl;
-  if (error = pthread_create(& thread_id, NULL, handle_data_requests, data_channel)) {
-    fprintf(stderr, "p_create failed: %s\n", strerror(error));
-  }  
-
-}
-
-/*--------------------------------------------------------------------------*/
-/* LOCAL FUNCTIONS -- THE PROCESS REQUEST LOOP */
-/*--------------------------------------------------------------------------*/
-
-void process_request(RequestChannel & _channel, const string & _request) {
-
-  if (_request.compare(0, 5, "hello") == 0) {
-    process_hello(_channel, _request);
-  }
-  else if (_request.compare(0, 4, "data") == 0) {
-    process_data(_channel, _request);
-  }
-  else if (_request.compare(0, 9, "newthread") == 0) {
-    process_newthread(_channel, _request);
-  }
-  else {
-    _channel.cwrite("unknown request");
-  }
-
-}
-
-void handle_process_loop(RequestChannel & _channel) {
-
-  for(;;) {
-
-    cout << "Reading next request from channel (" << _channel.name() << ") ..." << flush;
-    string request = _channel.cread();
-    cout << " done (" << _channel.name() << ")." << endl;
-    cout << "New request is " << request << endl;
-
-    if (request.compare("quit") == 0) {
-      _channel.cwrite("bye");
-      usleep(10000);          // give the other end a bit of time.
-      break;                  // break out of the loop;
-    }
-
-    process_request(_channel, request);
-  }
-  
-}
-
 /*--------------------------------------------------------------------------*/
 /* MAIN FUNCTION */
 /*--------------------------------------------------------------------------*/
@@ -204,10 +122,10 @@ void handle_process_loop(RequestChannel & _channel) {
 int main(int argc, char * argv[]) {
 
   //  cout << "Establishing control channel... " << flush;
-  RequestChannel control_channel("control", RequestChannel::SERVER_SIDE);
+  //NetworkRequestChannel control_channel("control", RequestChannel::SERVER_SIDE);
   //  cout << "done.\n" << flush;
 
-  handle_process_loop(control_channel);
+  //handle_process_loop(control_channel);
 
 }
 
